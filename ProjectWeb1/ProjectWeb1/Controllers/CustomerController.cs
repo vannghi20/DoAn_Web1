2485c984d9bf36d8b139a7699d6685abbc22a181
maitram1209 <maitram12092000@gmail.com>
2021-12-03 20:15:05 +0700

    CreateNew Customer

---
+++ ProjectWeb1/ProjectWeb1/Controllers/CustomerController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectWeb1.Interface;
+using ProjectWeb1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWeb1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        
+        private readonly ICustomerLogic _customerLogic;
+        public CustomerController(ICustomerLogic customerLogic)
+        {
+            _customerLogic = customerLogic;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAllCustomer()
+        {
+            try
+            {
+                var response = await _customerLogic.GetAllCustomer();
+                if (response != null && response.Count > 0)
+                {
+                    return Ok(response);
+                }
+                else
+                {
+                    return BadRequest(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+
+        }
+        // POST api/<UserController>---code của trâm
+        [HttpPost("add")]
+        public async Task<ActionResult> Register(Customer user)
+        {
+                if (ModelState.IsValid)
+                {
+                    
+                    var response = await _customerLogic.Register(user);
+                    return Ok(response);
+                }
+                else
+                {
+                    return BadRequest("Đăng kí thất bại");
+                }
+
+        }
+
+       
+    }
+}
---
+++ ProjectWeb1/ProjectWeb1/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWeb1.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [StringLength(255)]
+        [Required(ErrorMessage = "Please enter name")]
+        public string CustomerName { get; set; }
+        [Required(ErrorMessage = "Please enter phone number")]
+        [StringLength(10, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 10)]
+        [Phone]
+        public string CustomerPhone { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Passord")]
+        public string Password { get; set; }
+    }
+}
